8596e6f628ba372ff43953a2bcdc18eb5051501c
Nicholas Buzzerio <nick.buzzerio@hotmail.com>
2021-03-18 18:25:25 -0400

    fixed another bug in store model joi validation and refactored store model

--- models/store.js
+++ models/store.js
@@ -2,40 +2,38 @@ const Joi = require('joi');
 const mongoose = require('mongoose');
 
 const storeSchema = new mongoose.Schema({
-    store: {
-        name: {
-            type: String,
-            minlength: 5,
-            maxlength: 100,
-            required: true
-        },
-        location: {
-            type: String,
-            minlength: 5,
-            maxlength: 100,
-            required: true
-        },
-        nameAndLocation: {
-            type: String,
-            minlength: 10,
-            maxlength: 255,
-            required: true,
-            unique: true
-        },
-        isleLocation: {
-            type: Number,
-            required: true,
-            minlength: 0,
-            maxlength: 255,
-        }
+    name: {
+        type: String,
+        minlength: 5,
+        maxlength: 100,
+        required: true
+    },
+    location: {
+        type: String,
+        minlength: 5,
+        maxlength: 100,
+        required: true
+    },
+    name_Location: {
+        type: String,
+        minlength: 10,
+        maxlength: 255,
+        required: true,
+        unique: true
+    },
+    isleLocation: {
+        type: Number,
+        required: true,
+        minlength: 0,
+        maxlength: 255,
     }
 });
 
 function validateStore(store) {
     const schema = Joi.object({
-        name: Joi.string().min(5).max(100).required,
-        location: Joi.string().min(5).max(100).required,
-        isleLocation: Joi.number().min(0).max(255).required,
+        name: Joi.string().min(5).max(100).required(),
+        location: Joi.string().min(5).max(100).required(),
+        isleLocation: Joi.number().min(0).max(255).required(),
     });
     return schema.validate(store);
 }
