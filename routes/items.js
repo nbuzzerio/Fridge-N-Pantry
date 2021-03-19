7eb1b2b6d3cc7eb19183ee98c2e2318699f85e3f
Nicholas Buzzerio <nick.buzzerio@hotmail.com>
2021-03-19 19:53:15 -0400

    implemented put route to update an item's isleNumber in a specific store

--- routes/items.js
+++ routes/items.js
@@ -3,6 +3,7 @@ const express = require('express');
 const router = express.Router();
 const { Item, validateItem } = require("../models/item");
 const { validateStore } = require("../models/store");
+const _ = require('lodash');
 
 router.get('/:store', (req, res) => {
     res.send('These are all the items at a store');
@@ -27,25 +28,39 @@ router.post('/', async (req, res) => {
 
     let item = await Item.findOne({name: req.body.name});
     if (item) {
-        if (item.itemLocations.get(name_Location)) return res.status(400).send('Item is already registered to at that store');
+        if (item.itemLocations.get(name_Location)) return res.status(400).send('Item is already registered to at that store.');
 
         item.itemLocations.set(name_Location, store);
         await item.save()
 
-    } else {
-        item = new Item({
-            name: req.body.name,
-            itemLocations: {}
-        });
-        if (store) item.itemLocations.set(name_Location, store);
-        await item.save()
+        return res.send(item);
     }
 
-    res.send(item)
+    item = new Item({
+        name: req.body.name,
+        itemLocations: {}
+    });
+    if (store) item.itemLocations.set(name_Location, store);
+    await item.save();
+
+    res.send(item);
 });
 
-router.put('/:itemId/:storeName', (req, res) => {
-    res.send('This is the Items endpoint');
+router.put('/:id/:storeName/:isleNumber', async (req, res) => {
+    const { id, storeName, isleNumber} = req.params;
+    const item = await Item.findOne({ _id: id });
+    const store = item.itemLocations.get(storeName)._doc;
+    if (!item || !store) return res.status(400).send("Either item or store does not exist.");
+
+    store.isleLocation = isleNumber;
+    item.itemLocations.set(storeName, store);
+    const {error} = validateStore(_.pick(store, ['name', 'location', 'isleLocation']));
+    console.log(store)
+    if (error) return res.status(400).send(error.details[0].message);
+
+    await item.save();
+
+    res.send(item);
 });
 
 router.delete('/:itemId/:storeName', (req, res) => {
