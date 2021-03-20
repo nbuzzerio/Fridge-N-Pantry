983d8f2e382e211ab2146d5b25e8b03c60af7c32
Nicholas Buzzerio <nick.buzzerio@hotmail.com>
2021-03-20 14:00:38 -0400

    extracted mongoose out of index.js into its own database module in startup folder

--- index.js
+++ index.js
@@ -1,21 +1,17 @@
 require('dotenv').config()
-const mongoose = require('mongoose');
 const winston = require('winston');
 const express = require('express');
 const app = express();
 
 require('./startup/routes')(app);
+require('./startup/database')();
 
-winston.exceptions.handle(new winston.transports.File({ filename: 'uncaughtExceptions.log'}));
+winston.exceptions.handle(new winston.transports.File({ filename: './logs/uncaughtExceptions.log'}));
 process.on('unhandledRejection', (exception => {
     throw exception;
 }));
 
-winston.add(new winston.transports.File({ filename: 'logfile.log'}));
-
-mongoose.connect(process.env.DB_URL, {useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true})
-    .then(() => console.log('Connected to MongoDB...'))
-    .catch(err => console.error('Could not connect to MongoDB...'));
+winston.add(new winston.transports.File({ filename: './logs/logfile.log'}));
 
 const port = process.env.PORT || 3000;
 app.listen(port, () => console.log(`Listening on port ${port}...`));
