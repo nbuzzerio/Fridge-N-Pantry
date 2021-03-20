ef0557585436fc249a368a6ae9134c62d1800639
Nicholas Buzzerio <nick.buzzerio@hotmail.com>
2021-03-20 14:07:02 -0400

    extracted log transports out of index.js into their own logging module in startup folder

--- index.js
+++ index.js
@@ -1,17 +1,10 @@
-require('dotenv').config()
-const winston = require('winston');
+require('dotenv').config();
 const express = require('express');
 const app = express();
 
+require('./startup/logging')();
 require('./startup/routes')(app);
 require('./startup/database')();
-
-winston.exceptions.handle(new winston.transports.File({ filename: './logs/uncaughtExceptions.log'}));
-process.on('unhandledRejection', (exception => {
-    throw exception;
-}));
-
-winston.add(new winston.transports.File({ filename: './logs/logfile.log'}));
 
 const port = process.env.PORT || 3000;
 app.listen(port, () => console.log(`Listening on port ${port}...`));
---
+++ startup/logging.js
@@ -0,0 +1,10 @@
+const winston = require('winston');
+
+module.exports = function () {
+    winston.exceptions.handle(new winston.transports.File({ filename: './logs/uncaughtExceptions.log'}));
+    process.on('unhandledRejection', (exception => {
+        throw exception;
+    }));
+    
+    winston.add(new winston.transports.File({ filename: './logs/logfile.log'}));
+}
