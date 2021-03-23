4b6a14aa38930ff29706aa4cd85004713f2e9d18
Nicholas Buzzerio <nick.buzzerio@hotmail.com>
2021-03-22 21:28:35 -0400

    created tests for email and password validation in validateUser

--- tests/unit/models/users.test.js
+++ tests/unit/models/users.test.js
@@ -1,6 +1,7 @@
 const { User, validateUser } = require('../../../models/users');
 const jwt = require('jsonwebtoken');
 const mongoose = require('mongoose');
+const { before } = require('lodash');
 require('dotenv').config();
 
 describe('user.generateAuthToken', () => {
@@ -15,7 +16,10 @@ describe('user.generateAuthToken', () => {
 });
 
 describe('validateUser', () => {
-    const payload = {name: 'John', email: 'John@Doe.com', password: '12345' };
+    beforeEach(() => {
+        payload = {name: 'John', email: 'John@Doe.com', password: '12345' };
+    })
+    let payload;
 
     it('should return a valid user object', () => {
         expect(validateUser(payload).value).toMatchObject(payload);
@@ -28,5 +32,25 @@ describe('validateUser', () => {
         expect(validateUser(payload).error.details[0].message).toBe("\"name\" length must be less than or equal to 20 characters long");
         delete payload.name;
         expect(validateUser(payload).error.details[0].message).toBe("\"name\" is required");
+    });
+
+    it('should return an error if payload email is invalid or missing', () => {
+        payload.email = 'ThisIsNotAnEmailAddress';
+        expect(validateUser(payload).error.details[0].message).toBe("\"email\" must be a valid email");
+        payload.email = 'Jo@g';
+        expect(validateUser(payload).error.details[0].message).toBe("\"email\" length must be at least 5 characters long");
+        payload.email = Array(52).join('x');
+        expect(validateUser(payload).error.details[0].message).toBe("\"email\" length must be less than or equal to 50 characters long");
+        delete payload.email;
+        expect(validateUser(payload).error.details[0].message).toBe("\"email\" is required");
+    });
+
+    it('should return an error if payload password is invalid or missing', () => {
+        payload.password = '1234';
+        expect(validateUser(payload).error.details[0].message).toBe("\"password\" length must be at least 5 characters long");
+        payload.password = Array(1026).join('x');
+        expect(validateUser(payload).error.details[0].message).toBe("\"password\" length must be less than or equal to 1024 characters long");
+        delete payload.password;
+        expect(validateUser(payload).error.details[0].message).toBe("\"password\" is required");
     });
 });
